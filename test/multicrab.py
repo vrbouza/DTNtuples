16bcccd6930fad226fe5305fc945d5d30861be42
Victor Rodriguez Bouza <15350389+vrbouza@users.noreply.github.com>
2019-09-24 09:40:18 +0200

    ·enhanced some details

--- test/multicrab.py
+++ test/multicrab.py
@@ -1,4 +1,4 @@
-name      = 'DT_TDR_sep23'  #Part of the name of your output directory, adapt as needed.
+name      = 'DT_TDR_sep24_HBageingfixed'  #Part of the name of your output directory, adapt as needed.
 steam_dir = 'Summer16_FlatPU28to62/'  #Part of the name of your output directory, adapt as needed. 
 running_options = {
    'noage_norpc'            : ['useRPC=0'],
@@ -11,17 +11,17 @@ running_options = {
 runATCAF = False
 
 dataset = {
-   "nopu" : "/Mu_FlatPt2to100-pythia8-gun/PhaseIITDRSpring19DR-NoPU_106X_upgrade2023_realistic_v3-v1/GEN-SIM-DIGI-RAW",
+   "nopu"  : "/Mu_FlatPt2to100-pythia8-gun/PhaseIITDRSpring19DR-NoPU_106X_upgrade2023_realistic_v3-v1/GEN-SIM-DIGI-RAW",
    "pu200" : "/Mu_FlatPt2to100-pythia8-gun/PhaseIITDRSpring19DR-PU200_106X_upgrade2023_realistic_v3-v2/GEN-SIM-DIGI-RAW",
 }
 
 listOfOptions = [
-  'noage_norpc'   ,
-#   'age_norpc'     ,
+  #'noage_norpc'   ,
 #   'noage_withrpc' ,
+  #'age_norpc'     ,
 #   'age_withrpc'   ,
-#   'age_norpc_youngseg',
-#   'age_withrpc_youngseg',
+  'age_norpc_youngseg',
+  'age_withrpc_youngseg',
 ]
 
 listOfSamples = [
@@ -40,18 +40,18 @@ if __name__ == '__main__':
    def submit(config):
        res = crabCommand('submit', config = config )
 
-   config.General.workArea = 'analysis_'+name
+   config.General.workArea     = 'analysis_' + name
    config.General.transferLogs = True
 
    config.JobType.pluginName = 'Analysis'
-   config.JobType.psetName = 'dtDpgNtuples_phase2_cfg.py' 
+   config.JobType.psetName   = 'dtDpgNtuples_phase2_cfg.py'
 
    config.JobType.maxMemoryMB = 2500
-   config.JobType.inputFiles = ['MuonSystemAging.db']
+   config.JobType.inputFiles  = ['MuonSystemAging.db']
    #config.JobType.allowUndistributedCMSSW = True
 
-   config.Data.inputDBS = 'global'
-   config.Data.splitting = 'FileBased'
+   config.Data.inputDBS    = 'global'
+   config.Data.splitting   = 'FileBased'
    config.Data.publication = True
    config.Data.unitsPerJob = 1
    #config.Data.outLFNDirBase = '/store/user/sesanche/DT_L1_TDR_12_09_19'
@@ -60,7 +60,7 @@ if __name__ == '__main__':
 
    #config.Site.storageSite = 'T2_ES_IFCA'
    config.Site.storageSite = 'T2_CH_CERN'
-   config.Site.blacklist = ['T2_BR_SPRACE', 'T2_US_Wisconsin', 'T1_RU_JINR', 'T2_RU_JINR', 'T2_EE_Estonia']
+   config.Site.blacklist   = ['T2_BR_SPRACE', 'T2_US_Wisconsin', 'T1_RU_JINR', 'T2_RU_JINR', 'T2_EE_Estonia']
    if runATCAF :
       config.Site.whitelist = ['T3_CH_CERN_CAF']
       config.Site.ignoreGlobalBlacklist = True
