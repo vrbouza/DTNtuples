c12e0727a9e1310431257870d7340a51c5c06c52
Victor Rodriguez Bouza <15350389+vrbouza@users.noreply.github.com>
2019-09-24 09:28:38 +0200

    Modified details and improved things

--- test/multicrab.py
+++ test/multicrab.py
@@ -1,7 +1,7 @@
-name = 'DT_TDR_sep16_vis'  #Part of the name of your output directory, adapt as needed.  
+name      = 'DT_TDR_sep23'  #Part of the name of your output directory, adapt as needed.
 steam_dir = 'Summer16_FlatPU28to62/'  #Part of the name of your output directory, adapt as needed. 
 running_options = {
-   'noage_norpc'   : ['useRPC=0'],
+   'noage_norpc'            : ['useRPC=0'],
    'age_norpc'              : ['applyTriggerAgeing=True', 'applySegmentAgeing=True', 'ageingInput=sqlite_file:MuonSystemAging.db','useRPC=0'],
    'age_norpc_youngseg'     : ['applyTriggerAgeing=True', 'ageingInput=sqlite_file:MuonSystemAging.db','useRPC=0'],
    'noage_withrpc'          : ['useRPC=1'],
@@ -16,18 +16,18 @@ dataset = {
 }
 
 listOfOptions = [
-#   'noage_norpc'   ,
+  'noage_norpc'   ,
 #   'age_norpc'     ,
 #   'noage_withrpc' ,
 #   'age_withrpc'   ,
-   'age_norpc_youngseg',
-   'age_withrpc_youngseg',
+#   'age_norpc_youngseg',
+#   'age_withrpc_youngseg',
 ]
 
 listOfSamples = [
 "nopu",
 "pu200",
-   ]
+]
 
 if __name__ == '__main__':
 
@@ -54,9 +54,12 @@ if __name__ == '__main__':
    config.Data.splitting = 'FileBased'
    config.Data.publication = True
    config.Data.unitsPerJob = 1
-   config.Data.outLFNDirBase = '/store/user/sesanche/DT_L1_TDR_12_09_19'
+   #config.Data.outLFNDirBase = '/store/user/sesanche/DT_L1_TDR_12_09_19'
+   #config.Data.outLFNDirBase = '/store/user/rodrigvi/'
+   config.Data.outLFNDirBase = '/store/group/phys_muon/rodrigvi/'
 
-   config.Site.storageSite = 'T2_ES_IFCA'
+   #config.Site.storageSite = 'T2_ES_IFCA'
+   config.Site.storageSite = 'T2_CH_CERN'
    config.Site.blacklist = ['T2_BR_SPRACE', 'T2_US_Wisconsin', 'T1_RU_JINR', 'T2_RU_JINR', 'T2_EE_Estonia']
    if runATCAF :
       config.Site.whitelist = ['T3_CH_CERN_CAF']
